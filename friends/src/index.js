7ee2a7ec3f36f4981a7b55488a84450cdf7868f6
trucane <trufashionista2013@gmail.com>
2019-06-20 15:59:24 -0500

    WIP created store, created simple reducer and export it, passed store to provider

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Import all install dependencies
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from "redux";
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import reducer from './reducers';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const store = createStore(reducer);
+
+
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
+
+//1. import all install dependencies
+//2. create store, add reducer as object
+//3. wrap App in Provider pass in store as a prop
---
+++ friends/src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+    friends:[]
+}
+
+
+const reducer = (state = initialState, action) => {}
+
+
+export default reducer
